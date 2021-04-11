9cc556db8aaad1a9d5bdc7b9cd26e61b9d609daf
Moaz <moaz.elsayed002@gmail.com>
2021-04-11 20:19:15 +0200

    Add ps0 assignment

---
+++ ps0/ps0.py
@@ -0,0 +1,9 @@
+
+import math
+
+first_number = int(input("Enter number X: "))
+second_number = int(input("Enter number Y: "))
+
+print(F"x ** Y = {first_number**second_number}")
+print(F"x // Y =  {first_number//second_number}")
+print(F"logarithm of “X” is = {math.log(first_number)}")
