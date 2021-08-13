8da71850d9347057f62dc6705896c5d7bb5b4e48
moaaz <moaz.elsayed002@gmail.com>
2021-08-13 19:16:37 +0200

    Update deal_hand() to support wildcard

--- PS3/ps3.py
+++ PS3/ps3.py
@@ -151,8 +151,8 @@ def deal_hand(n):
 
     hand = {}
     num_vowels = int(math.ceil(n / 3))
-
-    for i in range(num_vowels):
+    hand['*'] = 1
+    for i in range(num_vowels - 1):
         x = random.choice(VOWELS)
         hand[x] = hand.get(x, 0) + 1
 
