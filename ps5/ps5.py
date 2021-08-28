4c6d01fdbdb4c2b918d995ff1e2904233fc9abe0
moaaz <moaz.elsayed002@gmail.com>
2021-08-29 00:47:37 +0200

    Add Problem 4 & 5

--- ps5/ps5.py
+++ ps5/ps5.py
@@ -137,6 +137,11 @@ class TitleTrigger(PhraseTrigger):
 # Problem 4
 # TODO: DescriptionTrigger
 
+
+class DescriptionTrigger(PhraseTrigger):
+    def evaluate(self, story):
+        return self.is_phrase_in(story.get_description())
+
 # TIME TRIGGERS
 
 # Problem 5
@@ -145,6 +150,12 @@ class TitleTrigger(PhraseTrigger):
 #        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
 #        Convert time from string to a datetime before saving it as an attribute.
 
+
+class TimeTrigger(Trigger):
+    def __init__(self, time):
+        time = datetime.strptime(time, "%d %b %Y %H:%M:%S")
+        time = time.replace(tzinfo=pytz.timezone("EST"))
+        self.time = time
 # Problem 6
 # TODO: BeforeTrigger and AfterTrigger
 
